57cc7bd6acdcbfd99954e139829ad0c20675c4c4
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-05-08 08:45:34 +0100

    cache

--- mix.py
+++ mix.py
@@ -1,20 +1,64 @@
-import random
+import json
 import mashup
 import os
+import random
 
-files = os.listdir("music")
-# random.shuffle(files)
+try:
+    with open("data/songlist") as f:
+        songs = [line.strip() for line in f]
+except FileNotFoundError:
+    songs = []
+
+
+def find_files(dir):
+    out = []
+    for file in os.listdir(dir):
+        if not file.startswith("."):
+            path = f"{dir}/{file}"
+            if os.path.isdir(path):
+                out += find_files(path)
+            elif os.path.isfile(path) and file.endswith(".mp3"):
+                out.append(path)
+    return out
+
+
+for file in find_files("music"):
+    if file not in songs:
+        songs.append(file)
+        with open("data/songlist", "a") as f:
+            f.write(f"{file}\n")
+
+try:
+    with open("data/mixability") as f:
+        mixability = json.load(f)
+except FileNotFoundError:
+    mixability = {}
+
+
+def get_mixability(song1, song2):
+    global mixability
+    if song1 not in mixability:
+        mixability[song1] = {}
+    if song2 not in mixability[song1]:
+        m = mashup.Mixer(song1, song2)
+        m.load_songs()
+        m.analyse()
+        mixability[song1][song2] = m.mixability
+
+        with open("data/mixability", "w") as f:
+            json.dump(mixability, f)
+
+    return mixability[song1][song2]
+
 
 scores = []
 
-for f in files:
-    for g in files:
-        if f != g:
-            m = mashup.Mixer(f"music/{f}", f"music/{g}")
-            m.load_songs()
-            m.analyse()
-            if m.mixability > 0:
-                scores.append((f"music/{f}", f"music/{g}", m.mixability))
+for s1 in songs:
+    for s2 in songs:
+        if s1 != s2:
+            m = get_mixability(s1, s2)
+            if m > 0:
+                scores.append((s1, s2, m))
                 print(scores[-1])
 
 scores.sort(key=lambda i: i[2])
@@ -23,13 +67,13 @@ for i, s in enumerate(scores[:5]):
     m = mashup.Mixer(s[0], s[1])
     m.load_songs()
     m.analyse()
-    m.mix()
+    m.mix(True)
     m.export(f"out/worst{i + 1}.mp3")
 
 for i, s in enumerate(scores[-5:]):
     m = mashup.Mixer(s[0], s[1])
     m.load_songs()
     m.analyse()
-    m.mix()
+    m.mix(True)
     m.export(f"out/best{5 - i}.mp3")
 
